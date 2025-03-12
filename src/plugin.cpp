8163afaf7cec72cdade8b4988b55541d0caa53d0
js <skylinertwo@aol.com>
2025-03-13 00:01:21 +0100

    formatting

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,38 +1,36 @@
 #include "log.h"
 
-
 void OnDataLoaded()
 {
-   
 }
 
-void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
+void MessageHandler(SKSE::MessagingInterface::Message *a_msg)
 {
-	switch (a_msg->type) {
+	switch (a_msg->type)
+	{
 	case SKSE::MessagingInterface::kDataLoaded:
-        
 		break;
 	case SKSE::MessagingInterface::kPostLoad:
 		break;
 	case SKSE::MessagingInterface::kPreLoadGame:
 		break;
 	case SKSE::MessagingInterface::kPostLoadGame:
-        break;
+		break;
 	case SKSE::MessagingInterface::kNewGame:
 		break;
 	}
 }
 
-SKSEPluginLoad(const SKSE::LoadInterface *skse) {
-    SKSE::Init(skse);
+SKSEPluginLoad(const SKSE::LoadInterface *skse)
+{
+	SKSE::Init(skse);
 	SetupLog();
 
-
-    auto messaging = SKSE::GetMessagingInterface();
-	if (!messaging->RegisterListener("SKSE", MessageHandler)) {
+	auto messaging = SKSE::GetMessagingInterface();
+	if (!messaging->RegisterListener("SKSE", MessageHandler))
+	{
 		return false;
 	}
 
-	
-    return true;
+	return true;
 }
