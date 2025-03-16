4630ab44e06f6a52d5d04e588cdbd13af068c5d1
js <skylinertwo@aol.com>
2025-03-16 01:00:43 +0100

    replace tabs with spaces

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -6,31 +6,31 @@ void OnDataLoaded()
 
 void MessageHandler(SKSE::MessagingInterface::Message *a_msg)
 {
-	switch (a_msg->type)
-	{
-	case SKSE::MessagingInterface::kDataLoaded:
-		break;
-	case SKSE::MessagingInterface::kPostLoad:
-		break;
-	case SKSE::MessagingInterface::kPreLoadGame:
-		break;
-	case SKSE::MessagingInterface::kPostLoadGame:
-		break;
-	case SKSE::MessagingInterface::kNewGame:
-		break;
-	}
+    switch (a_msg->type)
+    {
+    case SKSE::MessagingInterface::kDataLoaded:
+        break;
+    case SKSE::MessagingInterface::kPostLoad:
+        break;
+    case SKSE::MessagingInterface::kPreLoadGame:
+        break;
+    case SKSE::MessagingInterface::kPostLoadGame:
+        break;
+    case SKSE::MessagingInterface::kNewGame:
+        break;
+    }
 }
 
 SKSEPluginLoad(const SKSE::LoadInterface *skse)
 {
-	SKSE::Init(skse);
-	SetupLog();
+    SKSE::Init(skse);
+    SetupLog();
 
-	auto messaging = SKSE::GetMessagingInterface();
-	if (!messaging->RegisterListener("SKSE", MessageHandler))
-	{
-		return false;
-	}
+    auto messaging = SKSE::GetMessagingInterface();
+    if (!messaging->RegisterListener("SKSE", MessageHandler))
+    {
+        return false;
+    }
 
-	return true;
+    return true;
 }
